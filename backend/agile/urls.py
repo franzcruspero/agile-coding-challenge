057f751d70d04498369e76f6a1222db5ff29d49d
Franz Andrei Cruspero <franz.cruspero@gmail.com>
2021-08-17 00:38:46 +0800

    Add models, endpoints and add necessary libraries

--- backend/agile/urls.py
+++ backend/agile/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path(
+        "api/v1/",
+        include(
+            ("values_principles.api.v1.urls", "values_principles"),
+            namespace="values_principles-v1",
+        ),
+    ),
 ]
